55c7d31be37329ab0404e07bcc1b8d9ce0e69672
Andres Quinones <anguquga@gmail.com>
2020-04-11 20:35:47 -0500

    Seccion 8 Finalizada

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Aux from "../../../hoc/Aux";
+import React, {Component} from 'react';
+import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>
-        });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: </strong>{props.price.toFixed(2)}</p>
-            <p>Continue to Checkout?</p>
-            <Button clicked={props.cancel} btnType="Danger">CANCEL</Button>
-            <Button btnType="Success" clicked={props.checkout}>CONTINUE</Button>
-        </Aux>
-    );
-};
+class OrderSummary extends Component {
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            });
 
-export default orderSummary;
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: </strong>{this.props.price.toFixed(2)}</p>
+                <p>Continue to Checkout?</p>
+                <Button clicked={this.props.cancel} btnType="Danger">CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.checkout}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
+
+export default OrderSummary;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import logoBurger from '../../assets/images/27.1 burger-logo.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
-  <div className={classes.Logo} style={{height: props.height}}>
+  <div className={classes.Logo} style={{height:props.height}}>
       <img src={logoBurger} alt="My Burger" />
   </div>
 );
