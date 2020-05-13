298217e6ee408d8b57467a8c3464be5f60222b0f
anguquga <anguquga@gmail.com>
2020-05-13 18:50:55 -0500

    Video 7 Finalizado

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -3,14 +3,24 @@ import React from 'react';
 import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import NavigationItems from './NavigationItems';
-import navigationItem from './NavigationItem/NavigationItem';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    })
     it('should render 2 NavigationItems elements if not Authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
-        expect(wrapper.find(navigationItem)).toHaveLength(2);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
-    it('test', null);
+    it('should render 3 NavigationItems elements if Authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });    
+    it('should render 3 NavigationItems elements if Authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
